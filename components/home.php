64984123f1fb1258bb34566b62f116cf3b2d6fdd
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-07-01 18:40:02 +0530

    initial file structure and used pico CSS

---
+++ components/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
+</head>
+
+<body>
+    <nav class="container-fluid">
+        <ul>
+            <li><strong>Tours And Travel</strong></li>
+        </ul>
+        <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
